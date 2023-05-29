0053ffac0ec5e53fca562c91ff9a6d4a476ce52b
samazevedo <samuel.azevedo@live.com>
2023-05-29 15:56:07 -0700

    starting app

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ export const App: React.FC = () => {
     return (
         <>
             <h1>my app</h1>
+            <p>hello how are you doing</p>
         </>
     )
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,6 @@ import React from 'react'
 import { App } from './App'
 import { createRoot } from 'react-dom/client'
 
-const container = document.getElementById('app')
+const container = document.getElementById('root')
 const root = createRoot(container!)
 root.render(<App />)
